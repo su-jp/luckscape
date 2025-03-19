15b500ce5505696117ac5d93ca2b9db9eb1cd634
Sujin <su_jp@naver.com>
2025-03-19 12:48:03 +0900

    feat: 이용약관,개인정보처리방침,관리자문의 페이지 기획/구현
    feat: 이용약관,개인정보처리방침,관리자문의 페이지 기획/구현
    
    - 이용약관 내용 추가
    - 개인정보처리방침 내용 추가
    - 관리자문의 내용 추가
    
    refs [LSC-16]

--- luckscape/app/Views/contact.php
+++ luckscape/app/Views/contact.php
@@ -1,8 +1,5 @@
 <?php include(APPPATH . 'Views/layout/header.php'); ?>
-<div class="container">
-    <h2 class="mb-4">관리자 문의</h2>
-</div>
-
-<script>
-</script>
+    <h2 class="mb-4">📩 Contact Us</h2>
+    <p>문의사항이 있으시면 아래 이메일로 연락 주세요.</p>
+    <p>📧 <?= getenv('CONTACT_EMAIL') ?></p>
 <?php include(APPPATH . 'Views/layout/footer.php'); ?>
